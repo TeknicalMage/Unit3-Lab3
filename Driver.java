31398b17ffb919a2241d0c0593ab5a6d3dfecb6a
TeknicalMage <arcazjordan@gmail.com>
2018-10-16 09:10:53 -0400

    Create Driver.java

---
+++ Driver.java
@@ -0,0 +1,19 @@
+public class Driver {
+	//public static point a1 = new point();
+	//public static point b1 = new point();
+	public static LineSegment ls1 = new LineSegment();
+
+	
+	public Driver() {
+		main();
+	}
+	
+	public void main() {
+		ls1.main();
+	}
+
+	public static void main(String[] args) {
+		new Driver();
+
+	}
+}
