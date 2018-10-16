1ca1849f549d92237ba65086ab658178ad4ffead
TeknicalMage <arcazjordan@gmail.com>
2018-10-16 09:12:19 -0400

    Create point.java

---
+++ point.java
@@ -0,0 +1,34 @@
+public class point {
+	
+	private double x;
+	private double y;
+	
+	public double getX() {
+		return x;
+	}
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public void setY(double y) {
+		this.y = y;
+	}
+
+	public point() {		     
+	
+	}
+	
+	 public String toString() {
+		return "("+ x +", " + y + ")";
+		
+	}
+	
+	public static void main(String[] args) {
+		new point();
+	}
+}
